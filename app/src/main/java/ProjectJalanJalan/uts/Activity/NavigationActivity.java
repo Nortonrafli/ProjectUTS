b4b6c19f9dd5301e751468146aa058fb65db5372
nortonrafli <norton.nra27@gmail.com>
2019-10-16 08:35:11 +0700

    menambahkan code navigation activity dan welcome activity

--- app/src/main/java/ProjectJalanJalan/uts/Activity/NavigationActivity.java
+++ app/src/main/java/ProjectJalanJalan/uts/Activity/NavigationActivity.java
@@ -1,16 +1,52 @@
 package ProjectJalanJalan.uts.Activity;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import ProjectJalanJalan.uts.R;
-
-public class NavigationActivity extends AppCompatActivity {
+import ProjectJalanJalan.uts.Fragment.DownloadFragment;
+import ProjectJalanJalan.uts.Fragment.HomeFragment;
+public class NavigationActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.action_download:
+                fragment = new DownloadFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/ProjectJalanJalan/uts/Activity/WelcomeActivity.java
+++ app/src/main/java/ProjectJalanJalan/uts/Activity/WelcomeActivity.java
@@ -1,13 +1,31 @@
 package ProjectJalanJalan.uts.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import ProjectJalanJalan.uts.MainActivity;
 import ProjectJalanJalan.uts.R;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
